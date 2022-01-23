60a3528cd723bc4a9d07408dcbecd7564236cb1c
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2022-01-23 17:39:44 -0300

    Add files via upload

--- blitz/routes.js
+++ blitz/routes.js
@@ -4,6 +4,7 @@ const route = express.Router();
 
 const usuarioCtr = require('./src/ctr/usuarioCtr');
 const alertaCtr = require('./src/ctr/alertaCtr');
+const localCtr = require('./src/ctr/LocalCtr');
 
 route.post('/usuario', usuarioCtr.create);
 route.get('/usuario', usuarioCtr.read);
@@ -11,10 +12,16 @@ route.get('/usuario/:id', usuarioCtr.read);
 route.delete('/usuario/:id', usuarioCtr.remove);
 route.put('/usuario/:id', usuarioCtr.update);
 
+route.post('/alerta', alertaCtr.create);
 route.get('/alerta', alertaCtr.read);
 route.get('/alerta/:id', alertaCtr.read);
-route.post('/alerta', alertaCtr.create);
 route.put('/alerta/:id', alertaCtr.update);
 route.delete('/alerta/:id', alertaCtr.remove);
+
+route.post('/local', localCtr.create);
+route.get('/local', localCtr.read);
+route.get('/local/:id', localCtr.read);
+route.put('/local/:id', localCtr.update);
+route.delete('/local/:id', localCtr.remove);
 
 module.exports = route;
