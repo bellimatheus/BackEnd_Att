4ba356a50554e2db7ce97fa26b443c97abc060a6
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2022-01-27 11:13:38 -0300

    Add files via upload

---
+++ frontend/mapa/script.js
@@ -0,0 +1,36 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.querySelector(".map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 17,
+  });
+
+  map.addListener("click", (data) => {
+    console.log(data.latLng.lat(), data.latLng.lng() );
+    let coord = {lat: data.latLng.lat(), lng: data.latLng.lng() }
+
+    addMarker(coord, "Teste", "../assets/radar.png");
+  });
+
+
+  navigator.geolocation.getCurrentPosition((location) => {
+    console.log(location.coords.latitude, location.coords.longitude)
+    let coord = {lat: location.coords.latitude, lng: location.coords.longitude}
+
+    map.setCenter(coord);
+
+    addMarker(coord, "Minha localização", "../assets/radar.png");
+
+  })
+}
+
+function addMarker(coord, title, imagem){
+  new google.maps.Marker({
+    position: coord,
+    map: map,
+    title: title,
+    icon: imagem
+  });
+  
+}
