6e26ff1af669d9e74e0438f53175bc10cf2c8d3b
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2022-01-28 11:20:14 -0300

    Add files via upload

--- frontend/mapa/script.js
+++ frontend/mapa/script.js
@@ -1,5 +1,9 @@
-let map;
 
+const alerta = document.querySelector("#alerta");
+const cadastrar = document.querySelector("#cadastrar");
+const body = document.querySelector("body");
+
+let map;
 let CidadeAlerta;
 
 function initMap() {
@@ -14,6 +18,8 @@ function initMap() {
 
     // addMarker(coord, "Teste", "../assets/radar.png");
     CidadeAlerta = coord;
+    alerta.hidden = false
+    cadastrar.hidden = false
   });
 
 
@@ -28,6 +34,39 @@ function initMap() {
   })
 }
 
+function cadastro() {
+  let data = JSON.stringify({
+    "id_user" : 1,
+    "id_alerta" : alerta.value,
+    "coordenadas" : CidadeAlerta.lat + CidadeAlerta.lng,
+    "ativo" : true
+  })
+
+  fetch("http://localhost:3000/local", {
+    "method" : "POST",
+    "headers" : {
+      "Content-Type" : "application/json"
+    },
+    body: data
+  })
+
+  .then(resp => { return resp.json() })
+  .then(data => { 
+      if(data.id != undefined){
+        let tipo = alerta.value;
+        addMarker(CidadeAlerta, tipo, "../assets/radar.png");
+        alerta.hidden = true;
+        cadastrar.hidden = true;
+
+      }else{
+        alert("Falha encontrada em alerta")
+      }
+  })
+
+
+}
+
+
 function addMarker(coord, title, imagem){
   new google.maps.Marker({
     position: coord,
@@ -36,4 +75,45 @@ function addMarker(coord, title, imagem){
     icon: imagem
   });
   
+}
+
+function iniciar(){
+  CarrMarcadores();
+  CadAlerta();
+}
+
+
+
+function CarrMarcadores(){
+  fetch("http://localhost:3000/local")
+  .then(resp => { return resp.json()})
+  .then(data => {
+    data.forEach(localizacao => {
+      let coorden = localizacao.coordenadas.split(',');
+      let coord = {lat: Number(coorden[0]), lng:  Number(coorden[1])};
+
+      addMarker(coord, localizacao.alertum.tipo, "../assets/radar.png")
+    });
+  })
+
+
+
+
+}
+
+function CadAlerta(){
+  fetch("http://localhost:3000/alerta")
+
+  .then(resp => {
+    return resp.json();
+  })
+  .then(data => {
+    data.forEach(alert => {
+      let op = document.createElement("option");
+      op.value = alert.id;
+      op.innerHTML = alert.tipo;
+
+      alerta.appendChild(op);
+    });
+  })
 }
