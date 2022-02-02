1967da4eeeb4a2e2e5f6c72869b846a358adedef
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2022-02-01 21:23:19 -0300

    Add files via upload

--- frontend/usuario/script.js
+++ frontend/usuario/script.js
@@ -3,18 +3,21 @@ const TB = document.querySelector('tbody');
 
 function load() {
     carregarAlertas();
+    carregarMeusAlertas();
 }
 
 function carregarMeusAlertas(){
     let idUser = JSON.parse(localStorage.getItem('userdate').id)
+
     fetch("http://localhost:3000/local?id_user="+ idUser)
+
     .then(resp => {return resp.json()})
     .then (data => {
         data.forEach(local => {
-            let td = document.createElement("td");
-            let tdId = document.createElement("tr");
-            let tdCoord = document.createElement("tr");
-            let tdTipo = document.createElement("tr");
+            let tr = document.createElement("tr");
+            let tdId = document.createElement("td");
+            let tdCoord = document.createElement("td");
+            let tdTipo = document.createElement("td");
 
             tdId.innerHTML = local.id;
             tdCoord.innerHTML = local.coordenadas;
@@ -23,25 +26,27 @@ function carregarMeusAlertas(){
             tdId.innerHTML = local.id;
             tdId.innerHTML = local.coordenadas;
             tdId.style.maxWidth = "250px";
-            tdId.style.overflow = "break-word";
+            tdId.style.overflow = "word-break";
             tdId.innerHTML = local.alertum.tipo;
             
-            td.appendChild(tdId)
-            td.appendChild(tdCoord)
-            td.appendChild(tdTipo)
+            tr.appendChild(tdId)
+            tr.appendChild(tdCoord)
+            tr.appendChild(tdTipo)
 
-            TB.appendChild(td)
+            TB.appendChild(tr)
         })
     })
 }
 
 function carregarAlertas(){
     fetch("http://localhost:3000/alerta")
+    
     .then(resp => {return resp.json()})
     .then(data => {
         data.forEach(e => {
             let label = document.createElement("label");
             let checkbox = document.createElement("input");
+            
             label.innerHTML = e.tipo;
             label.for = e.id;
 
