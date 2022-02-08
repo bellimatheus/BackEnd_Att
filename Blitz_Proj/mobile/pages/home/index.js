f20a4a8135e66ba758558e83ec078a31b4ccdbef
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2022-02-08 11:18:40 -0300

    Add files via upload

--- Blitz_Proj/mobile/pages/home/index.js
+++ Blitz_Proj/mobile/pages/home/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
 
-export default function Home() {
+import {View, Dimensions, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function Home(){
+    const [coord, setCoord] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+    });
+
+
     return(
-        <View>
-            <Text>Tela Home</Text>
+        <View style={styles.container}>
+            <MapView style={styles.map} 
+            region = {{
+                ...coord, // latitude; coord.latitude ----- longitude: coord.longitude
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.05,
+              }}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
